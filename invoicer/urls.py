79a8ab43c6373bbc303eb70141f4ce54a9f903c5
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-10 12:30:12 -0300

    Implement user actions with djoser

--- invoicer/urls.py
+++ invoicer/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
